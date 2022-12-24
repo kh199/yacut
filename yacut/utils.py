98edd133bc72309182195285711a76c597a9447d
Екатерина Каричева <yek.cari4ewa@yandex.ru>
2022-12-24 04:38:43 +0300

    Fix pep8 error

--- yacut/utils.py
+++ yacut/utils.py
@@ -5,6 +5,7 @@ from .models import URLMap
 
 LINK_SYMBOLS = '^[A-Za-z0-9]*$'
 
+
 def get_unique_short_id():
     chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
     link = ''.join(random.choice(chars) for _ in range(6))
